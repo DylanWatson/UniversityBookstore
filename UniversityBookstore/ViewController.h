700d8ea1112f7ace0b653a26506eb8e1cbcf2363
Keyur Patel <patelplayer@gmail.com>
2014-10-31 19:37:58 -0400

    Initial commit of project.

---
+++ UniversityBookstore/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UniversityBookstore
+//
+//  Created by Keyur Patel on 10/31/14.
+//  Copyright (c) 2014 Keyur Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UniversityBookstore/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UniversityBookstore
+//
+//  Created by Keyur Patel on 10/31/14.
+//  Copyright (c) 2014 Keyur Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
