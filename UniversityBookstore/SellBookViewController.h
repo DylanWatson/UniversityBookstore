efe94f215dec8f61f97c0ff69be34685a4e4da6d
Keyur Patel <patelplayer@gmail.com>
2014-11-01 14:40:45 -0400

    Fixed more merge conflicts :)

--- UniversityBookstore/SellBookViewController.h
+++ UniversityBookstore/SellBookViewController.h
@@ -8,27 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-<<<<<<< HEAD
-@interface SellBookViewController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate, UIActionSheetDelegate>
-=======
 @interface SellBookViewController : UIViewController <UITextFieldDelegate, NSURLConnectionDelegate>
->>>>>>> 6699ca8804f9b2f425bc25d9bccf30d83ece8862
 {
     IBOutlet UITextField *nameOfBook;
     IBOutlet UITextField *isbn;
     IBOutlet UITextView *description;
     IBOutlet UITextField *price;
     IBOutlet UITextField *category;
-<<<<<<< HEAD
-=======
-    
->>>>>>> 6699ca8804f9b2f425bc25d9bccf30d83ece8862
 }
 - (IBAction)submit:(id)sender;
-
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 6699ca8804f9b2f425bc25d9bccf30d83ece8862
 @end
